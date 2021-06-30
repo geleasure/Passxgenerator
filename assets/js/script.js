75d44700b1e129f7ab56fb56bb5915573b7bb565
geleasure <grleasure@gmail.com>
2021-06-29 23:43:25 -0400

    added promp for password length 8-128 and promt if enter invalid length

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,24 @@ var generatePassword = function() {
         alert("Please select the correct options to create a strong password based on your requierments");
       }
     };
+
+
+    // prompt user for password length, checks for falsy or negative inputs (non-numeric values passed to Number() will return NaN)
+  while (true) {
+    askNumCharacters = Number(window.prompt("How many characters would you like to use?"));
+    if (askNumCharacters && askNumCharacters > 8 && askNumCharacters < 128) {
+      break;
+    }
+    else if (!askNumCharacters) {
+      alert("Try selecting just numbers");
+    }
+    else if (askNumCharacters <= 8) {
+      alert("Password must be more than 8 letters/characters");
+    }
+    else if (askNumCharacters > 128) {
+      alert("Password must be less than 128 letters/characters");
+    }
+    else {
+      alert("I don't know what you did wrong, and I don't think I really care at this point 😁")
+    }
+  }
